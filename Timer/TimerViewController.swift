3888451faf0bfa8cdfe2fc5c2bd124bc17d03e4d
Taylor Mott <taylor@mottapplications.com>
2015-10-16 23:36:38 -0600

    Initial Commit
    Signed-off-by: Taylor Mott <taylor@mottapplications.com>

---
+++ Timer/TimerViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TimerViewController.swift
+//  Timer
+//
+//  Updated by Taylor Mott on 10/16/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
